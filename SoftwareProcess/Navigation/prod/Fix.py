ca337a550fc99bb75a667a3d2dedb2180beaa9ab
Josh Starling <joshstarling92@gmail.com>
2016-10-09 08:42:33 -0500

    AT for __init__

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -1,3 +1,18 @@
+import time
 class Fix():
-    def __init__(self):
-        pass
+    def __init__(self,txtFile = None):
+        self.classErrorName = 'Fix.'
+        methodErrorName = '__init__:  ' #defined for error handling
+        if txtFile == None:
+            text_file = open("log.txt", "w")
+        else:
+            suffix = ".txt";
+            if txtFile.endswith(suffix):
+                text_file = open(txtFile, "w")
+            else:
+                errmsg = self.classErrorName + methodErrorName + 'Improper file extension used'
+                raise ValueError(errmsg)
+        text_file.write("LOG " + str(time.strftime("%Y/%m/%d")))    
+        text_file.close()
+        
+        
--- SoftwareProcess/Navigation/test/FixTest.py
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -1,3 +1,42 @@
 import Navigation.prod.Fix as Fix
+import unittest
+import os
+# ---- Acceptance Tests
+# 100 constructor
+#    Desired level of confidence:    boundary value analysis
+#    Input-output Analysis
+#        inputs:      n ->    integer .GE. 2 and .LT. 30  mandatory, unvalidated
+#        outputs:    instance of TCurve
+#    Happy path analysis:    
+#        n:      nominal value    n=4
+#                low bound        n=2
+#                high bound       n=29
+#    Sad path analysis:
+#        n:      non-int n          n="abc"
+#                out-of-bounds n    n=1; n=30
+#                missing n
+class FixTest(unittest.TestCase):
+# Happy path 
+#     def test100_010_ShouldConstruct(self):
+#         self.assertIsInstance(Fix.Fix(),Fix.Fix)
+        
+#     def test100_020_ShouldMakeTextFileWithGivenName(self):
+#         Fix.Fix()
+#         self.assertEqual(1, os.path.isfile('log.txt'))
+        
+    def test100_030_ShouldMakeTextFileWithGivenName(self):
+        Fix.Fix('log1.txt')
+        self.assertEqual(1, os.path.isfile('log1.txt'))
+#sad path
+    def test100_910_ShouldRaiseExceptionBadTxtFileName(self):
+        expectedString = "Fix.__init__:  Improper file extension used"
+        with self.assertRaises(ValueError) as context:
+            Fix.Fix('dsdd.tx')                           
+        self.assertEquals(expectedString, context.exception.args[0][0:len(expectedString)])
+# # Sad path  
+#     def test100_910_ShouldRaiseExceptionNoTxtFilePass(self):
+#         expectedString = "TCurve.__init__:"
+#         with self.assertRaises(ValueError) as context:
+#             T.TCurve("abc")                           
+#         self.assertEquals(expectedString, context.exception.args[0][0:len(expectedString)])  
 
-fix1 = Fix.Fix()
